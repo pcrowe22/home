51f820144d82cda0aa5c8610e37264a0dfd62d8b
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-11-13 16:28:06 -0600

    Got rid of extra if statement

--- javascript/script.js
+++ javascript/script.js
@@ -14,15 +14,3 @@ function displayNav() {
 		}
 	}
 }
-
-	if (!event.target.matches("#nav-button")) {
-		var dropdowns = document.getElementsByClassName("dropdownLinks");
-		var i;
-		for (i=0; i<dropdowns.length; i++){
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
