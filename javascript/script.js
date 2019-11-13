a24352e73584d3e4e4d16aca786a5bc52dafa94e
Patrick Crowe <pcrowe@hawk.iit.edu>
2019-11-13 16:15:51 -0600

    back to onclick

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,20 @@
+var isShowing = false;
 function displayNav() {
-	document.getElementById("linkListHolder").classList.toggle("show");
+	if (isShowing == true) {
+		document.getElementById("linkListHolder").classList.toggle("show");
+		isShowing = false;
+	} else {
+		var dropdowns = document.getElementsByClassName("dropdownLinks");
+		var i;
+		for (i=0; i<dropdowns.length; i++){
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
 }
-window.onclick = function(event) {
+
 	if (!event.target.matches("#nav-button")) {
 		var dropdowns = document.getElementsByClassName("dropdownLinks");
 		var i;
